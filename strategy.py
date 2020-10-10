158116c75287b8adfd429364c74d7dd71ab5849d
AlisaNovik <72654803+AlisaNovik@users.noreply.github.com>
2020-10-10 20:22:04 +0300

    Add files via upload

--- strategy.py
+++ strategy.py
@@ -5,22 +5,21 @@ class abstractStrategy(ABC):
     def play(self):
         pass
 class BaseStrategy(abstractStrategy):
-    class BaseStrategy:
-        def __init__(self, envelopes):
-            self.envelopes = envelopes
+    def __init__(self, envelopes):
+        self.envelopes = envelopes
 
-        def play(self):
-            last_en = self.envelopes[0]
-            for i in range(100):
-                if self.envelopes[i].used == False:
-                    print("the money in the envelope:" + self.envelopes[i].money)
-                    answer = input("do you want to keep this envelope - yes/no?")
-                    if answer == "no":
-                        last_en = self.envelopes[i]
-                    if answer == "yes":
-                        self.envelopes[i].used = True
-                        return self.envelopes[i]
-            return last_en
+    def play(self):
+        last_en = self.envelopes[0]
+        for i in range(100):
+            if self.envelopes[i].used == False:
+                print("the money in the envelope:" + self.envelopes[i].money)
+                self.envelopes[i].used = True
+                answer = input("do you want to keep this envelope - yes/no?")
+                if answer == "no":
+                    last_en = self.envelopes[i]
+                if answer == "yes":
+                    return self.envelopes[i]
+        return last_en
     def display(self):
         return "player chooses envelope"
 
@@ -39,7 +38,7 @@ class N_max_strategy(abstractStrategy):
             self.N = N
 
     def display(self):
-        return f"finding the first [{self.N}]'s envelopes with most money"
+        return f"finding the first [{self.N}] envelopes with most money"
 
     def play(self):
         max = 0  # most money
