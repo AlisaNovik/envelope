208e48eef07578936efc5e98d594fedd5869eff8
AlisaNovik <72654803+AlisaNovik@users.noreply.github.com>
2020-10-10 18:42:14 +0300

    Add files via upload

---
+++ envelope.py
@@ -0,0 +1,8 @@
+import random
+
+class Envelope:
+    def __init__(self):
+        self.money = random.randrange(500)
+        self.used = False
+
+
---
+++ strategy.py
@@ -0,0 +1,44 @@
+from  abc import ABC
+class abstractStrategy(ABC):
+    def display(self):
+        pass
+    def play(self):
+        pass
+class BaseStrategy(abstractStrategy):
+    def __init__(self, envelopes):
+        self.envelopes = envelopes
+
+
+class Automatic_BaseStrategy(abstractStrategy):
+    def __init__(self, envelopes):
+        self.envelopes = envelopes
+
+
+class N_max_strategy(abstractStrategy):
+    def __init__(self, envelopes, N = None):
+        self.envelopes = envelopes
+        if N is None:# set defualt
+            self.N = 3
+        else:
+            self.N = N
+
+    def display(self):
+        return f"finding the first [{self.N}]'s envelopes with most money"
+
+    def play(self):
+        max = 0  # most money
+        num = 0  # number of envelopes with more money then previous
+        for i in self.envelopes:
+            if num >= self.N:
+                break
+            if i > max:
+                max = i
+                num += 1
+            self.envelopes[i].used = True
+        return max
+
+
+class More_then_N_percent_group_strategy(abstractStrategy):
+    def __init__(self, envelopes, percent):
+        self.envelopes = envelopes
+        self.percent = percent
